8f8d818bd6b1481b333aac6094549be560617f6a
Manish Choudhary <choudharymanish@gmail.com>
2016-12-05 12:24:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/manishchoudhary/instaintegrationapp/example/ApplicationData.java
@@ -0,0 +1,11 @@
+package com.example.manishchoudhary.instaintegrationapp.example;
+
+/**
+ * Created by manish.choudhary on 12/2/2016.
+ */
+
+public class ApplicationData {
+    public static final String CLIENT_ID = "655cc45835d046c3bbb251a8a4dad825";
+    public static final String CLIENT_SECRET = "393083cff1fc4bd9bc6e9f8d387c5664";
+    public static final String CALLBACK_URL = "https://www.instagram.com/";
+}
